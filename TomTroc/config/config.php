b351b84c2eb58d9bbb8746dbf7059aae85abe0e3
Wutzy <33178404+Wutzy@users.noreply.github.com>
2024-09-21 17:45:56 +0200

    Update config.php
    Removed logs from config.php

--- TomTroc/config/config.php
+++ TomTroc/config/config.php
@@ -10,8 +10,8 @@
     define('TEMPLATE_VIEW_PATH', './views/templates/'); // Le chemin vers les templates de vues.
     define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php'); // Le chemin vers le template principal.
 
-    define('DB_HOST', 'localhost:8889');
-    define('DB_NAME', 'tomtroc');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'root');
+    define('DB_HOST', '');
+    define('DB_NAME', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
 
